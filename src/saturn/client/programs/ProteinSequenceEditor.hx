7bf0b989bf1a83f05898776ce8bd7c378abef8f7
Steve Joyce <stephen.joyce@ndorms.ox.ac.uk>
2018-02-13 15:23:24 +0000

    Updated authors and removed saveAll function (which is no longer needed)

--- src/saturn/client/programs/ProteinSequenceEditor.hx
+++ src/saturn/client/programs/ProteinSequenceEditor.hx
@@ -1,6 +1,6 @@
 /*
 * SATURN (Sequence Analysis Tool - Ultima regula natura)
-* Written in 2018 by David Damerell <david.damerell@sgc.ox.ac.uk>, Claire Strain-Damerell <claire.damerell@sgc.ox.ac.uk>, Brian Marsden <brian.marsden@sgc.ox.ac.uk>
+* Written in 2018 by David Damerell <david.damerell@sgc.ox.ac.uk>, Claire Strain-Damerell <claire.damerell@sgc.ox.ac.uk>, Stephen Joyce <stephen.joyce@ndorms.ox.ac.uk, Brian Marsden <brian.marsden@sgc.ox.ac.uk>
 *
 * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this
 * software to the public domain worldwide. This software is distributed without any warranty. You should have received a
@@ -592,21 +592,6 @@ class ProteinSequenceEditor implements SequenceChangeListener extends SequenceEd
             }
         }
     }
-	
-	override
-	public function saveAll() : Void {
-        var wo : Dynamic = getActiveObject(ProteinWorkspaceObject);
-
-        var prot : Protein;
-
-        if(Std.is(wo, saturn.core.Protein)){
-            prot = wo;
-        }else{
-            prot = wo.object;
-        }
-		
-		prot.setSequence(sequence);
-	}
 	
 	override
 	public function openFile(file : Dynamic, asNew : Bool, ?asNewOpenProgram : Bool = true) : Void {
--- src/saturn/core/Protein.hx
+++ src/saturn/core/Protein.hx
@@ -1,6 +1,6 @@
 /*
 * SATURN (Sequence Analysis Tool - Ultima regula natura)
-* Written in 2018 by David Damerell <david.damerell@sgc.ox.ac.uk>, Claire Strain-Damerell <claire.damerell@sgc.ox.ac.uk>, Brian Marsden <brian.marsden@sgc.ox.ac.uk>
+* Written in 2018 by David Damerell <david.damerell@sgc.ox.ac.uk>, Claire Strain-Damerell <claire.damerell@sgc.ox.ac.uk>, Stephen Joyce <stephen.joyce@ndorms.ox.ac.uk, Brian Marsden <brian.marsden@sgc.ox.ac.uk>
 *
 * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this
 * software to the public domain worldwide. This software is distributed without any warranty. You should have received a
