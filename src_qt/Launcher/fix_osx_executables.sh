e3817e2d183e450b0c91a3094d2977c2f3575b4f
David Damerell <david.damerell@sgc.ox.ac.uk>
2018-03-22 14:58:23 +0000

    Added library location rewrite commands for QtWebEngine plugin

--- src_qt/Launcher/fix_osx_executables.sh
+++ src_qt/Launcher/fix_osx_executables.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cd ../build-Saturn-Desktop_Qt_5_10_1_clang_64bit-Release
+#cd ../build-Saturn-Desktop_Qt_5_10_1_clang_64bit-Release
 
-cp -r ../build-filereader-Desktop_Qt_5_10_1_clang_64bit-Release/File/ Saturn.app/Contents/MacOS
-~/Qt/5.10.1/clang_64/bin/macdeployqt Saturn.app/ -executable=Saturn.app/Contents/MacOS/Saturn -qmldir=../Launcher/ -verbose=3
+#cp -r ../build-filereader-Desktop_Qt_5_10_1_clang_64bit-Release/File/ Saturn.app/Contents/MacOS
+#~/Qt/5.10.1/clang_64/bin/macdeployqt Saturn.app/ -executable=Saturn.app/Contents/MacOS/Saturn -qmldir=../Launcher/ -verbose=3
 
 install_name_tool -change @rpath/QtWebEngineWidgets.framework/Versions/5/QtWebEngineWidgets @executable_path/../frameworks/QtWebEngineWidgets.framework/Versions/5/QtWebEngineWidgets Saturn.app/Contents/MacOS/Saturn
 
@@ -48,3 +48,29 @@ install_name_tool -change @rpath/QtGui.framework/Versions/5/QtGui @executable_pa
 install_name_tool -change @rpath/QtWebEngine.framework/Versions/5/QtWebEngine @executable_path/../frameworks/QtWebEngine.framework/Versions/5/QtWebEngine Saturn.app/Contents/PlugIns/quick/libqtwebengineplugin.dylib
 
 install_name_tool -change @rpath/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../frameworks/QtWidgets.framework/Versions/5/QtWidgets Saturn.app/Contents/PlugIns/quick/libqtwebengineplugin.dylib
+
+##
+
+install_name_tool -change @rpath/QtWebEngineWidgets.framework/Versions/5/QtWebEngineWidgets @executable_path/../../../../../QtWebEngineWidgets.framework/Versions/5/QtWebEngineWidgets Saturn.app/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess 
+
+install_name_tool -change @rpath/QtWebEngineCore.framework/Versions/5/QtWebEngineCore @executable_path/../../../../../QtWebEngineCore.framework/Versions/5/QtWebEngineCore Saturn.app/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess
+
+
+install_name_tool -change @rpath/QtQuick.framework/Versions/5/QtQuick @executable_path/../../../../..//QtQuick.framework/Versions/5/QtQuick Saturn.app/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess
+
+install_name_tool -change @rpath/QtQml.framework/Versions/5/QtQml @executable_path/../../../../..//QtQml.framework/Versions/5/QtQml Saturn.app/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess
+
+install_name_tool -change @rpath/QtNetwork.framework/Versions/5/QtNetwork @executable_path/../../../../..//QtNetwork.framework/Versions/5/QtNetwork Saturn.app/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess
+
+install_name_tool -change @rpath/QtCore.framework/Versions/5/QtCore @executable_path/../../../../../QtCore.framework/Versions/5/QtCore Saturn.app/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess
+
+install_name_tool -change @rpath/QtWebChannel.framework/Versions/5/QtWebChannel @executable_path/../../../../../QtWebChannel.framework/Versions/5/QtWebChannel Saturn.app/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess
+
+
+install_name_tool -change @rpath/QtPositioning.framework/Versions/5/QtPositioning @executable_path/../../../../../QtPositioning.framework/Versions/5/QtPositioning Saturn.app/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess
+
+install_name_tool -change @rpath/QtGui.framework/Versions/5/QtGui @executable_path/../../../../../QtGui.framework/Versions/5/QtGui Saturn.app/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess
+
+install_name_tool -change @rpath/QtWebEngine.framework/Versions/5/QtWebEngine @executable_path/../../../../../QtWebEngine.framework/Versions/5/QtWebEngine Saturn.app/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess
+
+install_name_tool -change @rpath/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../../../../../QtWidgets.framework/Versions/5/QtWidgets Saturn.app/Contents/Frameworks/QtWebEngineCore.framework/Helpers/QtWebEngineProcess.app/Contents/MacOS/QtWebEngineProcess
