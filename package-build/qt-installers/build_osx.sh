baaceb1aea61fb9eff597effdaaf3875f085858d
David Damerell <david.damerell@sgc.ox.ac.uk>
2018-03-27 20:36:28 +0100

    OSX build updates

--- package-build/qt-installers/build_osx.sh
+++ package-build/qt-installers/build_osx.sh
@@ -26,11 +26,11 @@ cp packages/osx.single.app.x86_64/SATurnLaunch packages/osx.single.app.x86_64/da
 cp packages/osx.single.app.x86_64/Info.plist packages/osx.single.app.x86_64/data/SATurn.app/Contents
 cp packages/osx.single.app.x86_64/saturn.icns packages/osx.single.app.x86_64/data/SATurn.app/Contents/Resources
 
-/Users/sgcadmin/Qt/QtIFW-3.0.1/bin/binarycreator -c config/config.xml -p packages SATurnOSX --exclude osx.developer_tools.x86_64,osx.haxe.x86_64,osx.qt.x86_64,osx.tools.x86_64,osx.server.x86_64,common.saturn,windows.developer_tools.x86_64,windows.haxe.x86_64,windows.tools.x86_64,windows.qt.x86_64,windows.server.x86_64,linux.qt.x86_64,common.git.saturn,windows.haxe.x86_64,windows.developer_tools.x86_64,common.docs.saturn
+/Users/sgcadmin/Qt/QtIFW-3.0.1/bin/binarycreator -c config/osx.config.xml -p packages SATurnOSX --exclude osx.developer_tools.x86_64,osx.haxe.x86_64,osx.qt.x86_64,osx.tools.x86_64,osx.server.x86_64,common.saturn,windows.developer_tools.x86_64,windows.haxe.x86_64,windows.tools.x86_64,windows.qt.x86_64,windows.server.x86_64,linux.qt.x86_64,common.git.saturn,windows.haxe.x86_64,windows.developer_tools.x86_64,common.docs.saturn
 
-/Users/sgcadmin/Qt/QtIFW-3.0.1/bin/binarycreator -c config/config.xml -p packages SATurnNonStandardOSX --exclude osx.developer_tools.x86_64,osx.haxe.x86_64,windows.developer_tools.x86_64,windows.haxe.x86_64,windows.tools.x86_64,windows.qt.x86_64,windows.server.x86_64,linux.qt.x86_64,common.git.saturn,windows.haxe.x86_64,windows.developer_tools.x86_64,common.docs.saturn,osx.single.app.x86_64
+/Users/sgcadmin/Qt/QtIFW-3.0.1/bin/binarycreator -c config/osx.config.xml -p packages SATurnNonStandardOSX --exclude osx.developer_tools.x86_64,osx.haxe.x86_64,windows.developer_tools.x86_64,windows.haxe.x86_64,windows.tools.x86_64,windows.qt.x86_64,windows.server.x86_64,linux.qt.x86_64,common.git.saturn,windows.haxe.x86_64,windows.developer_tools.x86_64,common.docs.saturn,osx.single.app.x86_64
 cd packages/common.git.saturn
 rm -rf data
 git clone https://github.com/ddamerell53/SATurn.git data
 cd ../../
-/Users/sgcadmin/Qt/QtIFW-3.0.1/bin/binarycreator -c config/config.xml -p packages SATurnOSXDeveloper --exclude linux.qt.x86_64,windows.developer_tools.x86_64,windows.haxe.x86_64,windows.tools.x86_64,windows.qt.x86_64,windows.server.x86_64,osx.single.app.x86_64
+/Users/sgcadmin/Qt/QtIFW-3.0.1/bin/binarycreator -c config/osx.config.xml -p packages SATurnOSXDeveloper --exclude linux.qt.x86_64,windows.developer_tools.x86_64,windows.haxe.x86_64,windows.tools.x86_64,windows.qt.x86_64,windows.server.x86_64,osx.single.app.x86_64
