5fcbd14023c612563f60b536884d2dd8255c5b4f
David Damerell <david.damerell@sgc.ox.ac.uk>
2018-08-31 12:52:31 +0100

    [ChromoHub] Colour subfamilies as done in Lihua's prototype UbiHub interface

---
+++ src/saturn/client/programs/chromohub/annotations/AnnotationColourConstants.hx
@@ -0,0 +1,39 @@
+package saturn.client.programs.chromohub.annotations;
+class AnnotationColourConstants {
+    public static var black = 'rgb(0,0,0)';
+    public static var white = 'rgb(255,255,255)';
+    public static var grey = 'rgb(220,220,220)';
+    public static var brown = 'rgb(128,64,0)';
+    public static var lightBlue = 'rgb(129,190,247)';
+    public static var blue = 'rgb(41,128,214)';
+    public static var pink = 'rgb(192,86,145)';
+    public static var green = 'rgb(99,207,27)';
+    public static var orange = 'rgb(255,128,0)';
+    public static var purple = 'rgb(121,63,243)';
+    public static var red = 'rgb(191,0,0)';
+    public static var yellow = 'rgb(255,204,0)';
+    public static var darkGreen = 'rgb(0,64,64)';
+    public static var fGreen = 'rgb(0,128,64)';
+    public static var ferGreen = 'rgb(0,200,64)';
+    public static var darkBlue = 'rgb(0,0,255)';
+    public static var purpleBlue = 'rgb(92,92,220)';
+    public static var hotPink = 'rgb(255,0,128)';
+    public static var olive = 'rgb(128,128,0)';
+    public static var lGreen = 'rgb(0,255,0)';
+    public static var purple2 = 'rgb(128,0,128)';
+    public static var dGrey = 'rgb(128,128,128)';
+    public static var lRed = 'rgb(255,0,0)';
+    public static var purple3 = 'rgb(128,0,255)';
+    public static var dBrown = 'rgb(79,26,0)';
+    public static var darkOrange = 'rgb(172,91,10)';
+    public static var lightorange = 'rgb(255,141,27)';
+    public static var red2 = 'rgb(100,0,0)';
+    public static var babyBlue = 'rgb(102,255,255)';
+    public static var orangeHeader  =  'rgb(220,128,48)';
+    public static var greenBorder  =  'rgb(0,114,106)';
+
+
+    public function new() {
+
+    }
+}
