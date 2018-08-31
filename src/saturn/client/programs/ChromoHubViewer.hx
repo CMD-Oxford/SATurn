141096d01abad02445bfe0303c70693181cde998
Leonidas <leonidas.koukouflis@sgc.ox.ac.uk>
2018-08-31 16:18:06 +0100

    [ChromoHub] Fixed tree buttons

--- src/saturn/client/programs/ChromoHubViewer.hx
+++ src/saturn/client/programs/ChromoHubViewer.hx
@@ -1903,7 +1903,11 @@ class ChromoHubViewer  extends SimpleExtJSProgram  {
                 listeners:{
                     mouseover:
                     function(e){
-                        var xposition = e.pageX;
+
+                        var ev : Dynamic;
+                        var d : Dynamic = js.Browser.window;
+                        ev = d.event;
+                        var xposition: Dynamic = ev.pageX;
 
                         getApplication().getSingleAppContainer().showExportSubMenu(xposition);
                     }
