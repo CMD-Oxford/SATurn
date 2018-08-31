5fd73591c7568236367dffb4b95280834ab1d952
David Damerell <david.damerell@sgc.ox.ac.uk>
2018-08-31 10:57:00 +0100

    [ChromoHub] Added annotations for protein levels in normal tissues based on a percentage score

--- src/saturn/client/programs/chromohub/annotations/ProteinLevelNormalTissuesAnnotation.hx
+++ src/saturn/client/programs/chromohub/annotations/ProteinLevelNormalTissuesAnnotation.hx
@@ -105,4 +105,92 @@ class ProteinLevelNormalTissuesAnnotation {
             }
         });
     }
+
+    static function hasNormalLevelPercentage(target: String, data: Dynamic, selected:Int,annotList:Array<ChromoHubAnnotation>, item:String, callBack : HasAnnotationType->Void){
+        // TODO: To be implemented by Leo
+        var r : HasAnnotationType = {hasAnnot: true, text:'',color:{color:'#800080',used:true},defImage:0};
+
+        callBack(r);
+    }
+
+    static function hasNormalLevelPercentageFunction (annotation : Int, form : Dynamic, tree_type : String, family : String, searchGenes : Array<Dynamic>, viewer : ChromoHubViewer, cb : Dynamic->String->Void){
+
+        var proteinLevels = [];
+        var reliability = [];
+        var percentage = null;
+
+        if(form != null){
+            // We get here for tree annotation requests
+
+            // Process protein levels
+            if(form.form.findField('protein_level_high').lastValue){
+                proteinLevels.push('High');
+            }
+
+            if(form.form.findField('protein_level_medium').lastValue){
+                proteinLevels.push('Medium');
+            }
+
+            if(form.form.findField('protein_level_low').lastValue){
+                proteinLevels.push('Low');
+            }
+
+            // Process reliability
+            if(form.form.findField('protein_reliability_approved').lastValue){
+                reliability.push('Approved');
+            }
+
+            if(form.form.findField('protein_reliability_enhanced').lastValue){
+                reliability.push('Enhanced');
+            }
+
+            if(form.form.findField('protein_reliability_supported').lastValue){
+                reliability.push('Supported');
+            }
+
+            if(form.form.findField('protein_reliability_uncertain').lastValue){
+                reliability.push('Uncertain');
+            }
+
+            percentage = form.form.findField('in_percentage').lastValue;
+
+        }else{
+            // We aren't planning to support table view for these UbiHub specific annotations
+            throw new saturn.util.HaxeException('Table view not supported for ProteinLevelNormalTissueAnnotation');
+        }
+
+        // Prepare web-service call
+        var args = [{
+            'treeType' : tree_type, 'familyTree' : family,
+            'in_percentage': percentage,
+            'searchGenes' : searchGenes, 'protein_levels' :  proteinLevels,
+            'reliabilities': reliability
+        }];
+
+        // Make web-service call
+        WorkspaceApplication.getApplication().getProvider().getByNamedQuery('hookHasProteinNormalLevelsPercentage', args, null, false, function(db_results, error){
+            if(error == null){
+                if(db_results != null){
+
+                    viewer.activeAnnotation[annotation] = true;
+
+                    if(viewer.treeName == ''){
+                        // We get here for table view
+                        viewer.addAnnotDataGenes(db_results, annotation, function(){
+                            cb(db_results, null);
+                        });
+                    }else{
+                        // We get here for tree view
+                        viewer.addAnnotData(db_results, annotation, 0, function(){
+                            viewer.newposition(0, 0);
+
+                            cb(db_results, null);
+                        });
+                    }
+                }
+            }else{
+                cb(null,error);
+            }
+        });
+    }
 }
