3a92b9d8cfaa1cf584663d420307d785ad28bf8e
David Damerell <david.damerell@sgc.ox.ac.uk>
2019-01-11 14:16:33 +0000

    [Docker] Added new service configuration for Docker

--- build/rundockersaturn.sh
+++ build/rundockersaturn.sh
@@ -1,4 +1,4 @@
 export DEBUG=*
 cd /home/saturn/SATurn/build
 bin/redis/redis-4.0.12/src/redis-server&
-node SaturnServer.js services/ServicesLocalLiteLinux.json
+node SaturnServer.js services/DockerConfig.json
