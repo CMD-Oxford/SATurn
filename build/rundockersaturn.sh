09ff61f8d6965234af25fc3197a2f6f93010ef20
David Damerell <david.damerell@sgc.ox.ac.uk>
2019-02-11 13:51:55 +0000

    [Docker] Added NVM ENV config to start-up script

--- build/rundockersaturn.sh
+++ build/rundockersaturn.sh
@@ -1,3 +1,7 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 export DEBUG=*
 cd /home/saturn/SATurn/build
 bin/redis/redis-4.0.12/src/redis-server&
